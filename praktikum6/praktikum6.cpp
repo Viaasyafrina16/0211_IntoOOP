b2297df6deec08bf539d1688139db94d8eaafde5
Viaasyafrina16 <syafrinavia@gmail.com>
2024-04-23 15:27:53 +0700

    membuat akses modifier yang bersifat public untuk member nama,umur,jurusan di class mahasiswa

--- praktikum6/praktikum6.cpp
+++ praktikum6/praktikum6.cpp
@@ -15,25 +15,40 @@ public: // akses modifier
 };
 
 
-class Matakuliah{
-	private:
-		string kodeMK;
-		string namaMk;
-		int sks;
-	public :
-		void input() {
-			cout << "kode MK: ";
-			cin >> kodeMK;
-			cout << "Nama MK: ";
-			cin >> namaMk;
-			cout << "SKS: ";
-			cin >> sks;
-		}
+class Matakuliah {
+private:
+	string kodeMK;
+	string namaMk;
+	int sks;
+public:
+	void input() {
+		cout << "kode MK: ";
+		cin >> kodeMK;
+		cout << "Nama MK: ";
+		cin >> namaMk;
+		cout << "SKS: ";
+		cin >> sks;
+	}
 
-		void output(){
-			cout << "Kode MK: " << kodeMK << endl;
-			cout << "Nama MK: " << namaMk << endl;
-			cout << "sks: " << sks << endl;
+	void output() {
+		cout << "Kode MK: " << kodeMK << endl;
+		cout << "Nama MK: " << namaMk << endl;
+		cout << "sks: " << sks << endl;
+	}
+};
+
+int main() {
+	class Mahasiswa {
+	public: // akses modifier
+		string nama;
+		int umur;
+		string jurusan;
+		void output() {
+			cout << "Nama: " << nama << endl;
+			cout << "Umur: " << umur << endl;
+			cout << " jurusan: " << jurusan << endl;
+		}
+	};
 
 
 
