08197dc60a7cf6d751135a0ede586e4c3aad4935
Viaasyafrina16 <syafrinavia@gmail.com>
2024-04-23 14:58:06 +0700

    membuat fungsi output

--- praktikum6/praktikum6.cpp
+++ praktikum6/praktikum6.cpp
@@ -1,9 +1,17 @@
+// OOP 1
 #include <iostream>
 using namespace std;
 
-class Mahasiswa{
+class Mahasiswa {
 public: // akses modifier
 	string nama;
 	int umur;
 	string jurusan;
+	void output() {
+		cout << "Nama: " << nama << endl;
+		cout << "Umur: " << umur << endl;
+		cout << " jurusan: " << jurusan << endl;
+	}
+};
+
 
