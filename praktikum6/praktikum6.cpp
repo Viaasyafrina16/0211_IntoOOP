5ac8a596cadff8cf2e01c209d72c587190027c56
Viaasyafrina16 <syafrinavia@gmail.com>
2024-04-23 16:25:32 +0700

    membuat void display

--- praktikum6/praktikum6.cpp
+++ praktikum6/praktikum6.cpp
@@ -76,6 +76,13 @@ public:
 		return panjang * Lebar;
 	}
 
+	void display() {
+		cout << "Panjangnya = " << panjang << endl;
+		cout << "Lebarnya = " << Lebar << endl;
+		cout << "Luasnya = " << hitungLuas << endl;
+
+	}
+};
 
 
 
